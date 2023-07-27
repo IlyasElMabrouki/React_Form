990d88b301ae58d4141149243bb7cc1af216eecb
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-07-27 17:58:46 +0100

    implemented Hook Form and Devtools Visualisation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import {useForm} from "react-hook-form"
+import { DevTool } from '@hookform/devtools'
 
 export const YoutubeForm = ()=> {
+  const form = useForm()
+  const {register, control} = form
+
   return (
     <div>
         <form>
             <label htmlFor='username'>Username</label>
-            <input type='text' id='username' name='username'/>
+            <input type='text' id='username' {...register("username")}/>
 
             <label htmlFor='email'>Email</label>
-            <input type='text' id='email' name='email'/>
+            <input type='text' id='email' {...register("email")}/>
 
             <label htmlFor='channel'>Channel</label>
-            <input type='text' id='channel' name='channel'/>
+            <input type='text' id='channel' {...register("channel")}/>
 
             <button>Submit</button>
         </form>
+        <DevTool control={control} />
     </div>
   )
 }
+
+
